20684740f1988f4a45cb7fa4bf32537e57c46a8b
connorbanderson <connorbanderson@gmail.com>
2019-07-21 23:22:08 -0600

    New Portfolio Modal Component

---
+++ src/Components/NewPortfolioModal/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import Input from "../Input/input.js";
+import Modal from "../Modal";
+import { createPortfolio } from "../../Redux/Actions/portfolios";
+
+const NewPortfolioModal = ({ user, isVisible, toggleVisible }) => {
+  const [newPortfolioName, updatePortfolioName] = useState("");
+  const handleAddNewPortfolio = () => {
+    const portfolioInfo = {
+      name: newPortfolioName,
+      coins: {}
+    };
+    createPortfolio(user.uid, portfolioInfo);
+    toggleVisible();
+  };
+  return (
+    <Modal
+      isVisible={isVisible}
+      toggleModal={() => this.toggleVisible()}
+      title="Add a New Portfolio"
+    >
+      <Input
+        color="secondary"
+        autoFocus
+        label="Name"
+        value={newPortfolioName}
+        onChange={name => updatePortfolioName(name)}
+        handleSubmit={e => [e.preventDefault(), handleAddNewPortfolio()]}
+        error={null}
+      />
+    </Modal>
+  );
+};
+
+export default NewPortfolioModal;
