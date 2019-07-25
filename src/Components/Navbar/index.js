88f9a3bba81b9ad8e3395070f86ebb67f32c46ef
connorbanderson <connorbanderson@gmail.com>
2019-07-24 22:53:49 -0600

    Navbar Code Cleanup - Functional & BEM Style

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,11 +1,10 @@
 // Frontend Lib Imports
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 
 // Component Imports
 import TextField from "@material-ui/core/TextField";
 import IconButton from "@material-ui/core/IconButton";
-import Avatar from "@material-ui/core/Avatar";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import Person from "@material-ui/icons/Person";
 import Menu from "@material-ui/core/Menu";
@@ -14,6 +13,8 @@ import ExitToApp from "@material-ui/icons/ExitToApp";
 import PersonOutline from "@material-ui/icons/PersonOutline";
 import Button from "@material-ui/core/Button";
 import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Avatar from "../Avatar";
+
 //Redux
 import { logout } from "../../Redux/Actions/auth";
 
@@ -24,87 +25,64 @@ import "./Navbar.scss";
 import logo from "../../logo.svg";
 
 //Constant Imports
-import { AVATAR_PALETTE_400, AVATAR_PALETTE_600, ONLY_INT_REGX } from '../../constants'
+import {
+  AVATAR_PALETTE_400,
+  AVATAR_PALETTE_600,
+  ONLY_INT_REGX
+} from "../../constants";
 
-class Navbar extends Component {
-  state = {
-    anchorEl: null
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-  handleClick = e => {
-    this.setState({ anchorEl: e.currentTarget });
-  };
-
-  render() {
-    const { anchorEl } = this.state;
-    const { user, logout } = this.props;
-    const avatarColorId =  user.uid.replace(ONLY_INT_REGX, '') % 14
-    return (
-      <nav className="dashboardNavbar">
-        <div className="navbarInnerWrapper">
-          <div className="lhs">
-            <img
-              src={logo}
-              alt="navbar logo"
-              style={{ height: "32px", width: "32px" }}
-            />
-          </div>
-          <div className="middle" />
-          <div className="rhs flexRight">
-            <Avatar
-              style={{ height: "32px", width: "32px", marginRight: "5px" }}
-              alt="Remy Sharp"
-              src={user.photoURL ? user.photoURL : "https://scontent.fyyc4-1.fna.fbcdn.net/v/t1.15752-9/53270755_401825197043838_4318937078982246400_n.png?_nc_cat=105&_nc_ht=scontent.fyyc4-1.fna&oh=9e34adadc4dcb7d326ef65afb4b902d7&oe=5D893ECD"}
-            />
-            <div className="flexColRight">
-              <b style={{ marginLeft: "5px", marginRight: "5px", opacity: 0.7}}>
-                {user.displayName !== null ? user.displayName : user.email}
-              </b>
-              <div className="flexLeft">
-                <div className="onlineDot" />
-                <small style={{ marginLeft: "5px", opacity: 0.5 }}>
-                  Online
-                </small>
-              </div>
-            </div>
-            <IconButton
-              aria-controls="simple-menu"
-              aria-haspopup="true"
-              onClick={this.handleClick}
-              aria-label="ExpandMore"
-              size="small"
-            >
-              <ExpandMore fontSize="inherit" />
-            </IconButton>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={this.handleClose}
-              style={{ marginTop: "30px" }}
-            >
-              <MenuItem disabled onClick={this.handleClose}>
-                <div className="flexLeft">
-                  <PersonOutline style={{ marginRight: "5px" }} /> Profile
-                </div>
-              </MenuItem>
-              <MenuItem onClick={()=>[this.handleClose, logout()]}>
-                <div className="flexLeft">
-                  <ExitToApp style={{ marginRight: "5px" }} />
-                  Logout
-                </div>
-              </MenuItem>
-            </Menu>
-          </div>
+const Navbar = ({ user, logout }) => {
+  const handleClose = () => updateAnchorEl(null);
+  const [anchorEl, updateAnchorEl] = useState(null);
+  const avatarColorId = user.uid.replace(ONLY_INT_REGX, "") % 14;
+  const userName = user.displayName !== null ? user.displayName : user.email;
+  return (
+    <nav className="navBar">
+      <div className="navBar__innerWrapper">
+        <div className="navBar__lhs">
+          <img src={logo} alt="servologo" className="navBar__logo" />
         </div>
-      </nav>
-    );
-  }
-}
+        <div className="navBar__rhs">
+          <Avatar user={user} />
+          <div className="navBar__userNameWrapper">
+            <b className="navBar__userName">{userName}</b>
+            <div className="navBar__statusWrapper">
+              <div className="navBar__onlineDot" />
+              <small className="navBar__status">Online</small>
+            </div>
+          </div>
+          <IconButton
+            onClick={e => updateAnchorEl(e.currentTarget)}
+            size="small"
+          >
+            <ExpandMore fontSize="inherit" />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            className="navBar__menu"
+          >
+            <MenuItem disabled onClick={handleClose}>
+              <div className="navBar__menuItemWrapper">
+                <PersonOutline className="navBar__menuItemIcon" />
+                Profile
+              </div>
+            </MenuItem>
+            <MenuItem onClick={() => [handleClose, logout()]}>
+              <div className="navBar__menuItemWrapper">
+                <ExitToApp className="navBar__menuItemIcon" />
+                Logout
+              </div>
+            </MenuItem>
+          </Menu>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 const mapStateToProps = state => ({
   user: state.auth.user
@@ -118,17 +96,3 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps
 )(Navbar);
-
-/*
-
-<Button
-  className="squareButton"
-  variant="contained"
-  size="small"
-  onClick={() => logout()}
-  style={{ backgroundColor: "white", width: "32px" }}
->
-  <ExitToApp style={{}} />
-</Button>
-
-*/
