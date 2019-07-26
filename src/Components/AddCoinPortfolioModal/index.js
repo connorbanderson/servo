8d97eaca85a376d4b2018c9cc95137489460d160
connorbanderson <connorbanderson@gmail.com>
2019-07-25 22:50:46 -0600

    Format AddCoin Component

--- src/Components/AddCoinPortfolioModal/index.js
+++ src/Components/AddCoinPortfolioModal/index.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Modal from "../Modal";
 import Input from "../Input/input.js";
+import { NUMBER_ONLY_REGX } from "../../constants";
 
-const AddCoinPortfolioModal = ({ isVisible, toggleVisible, coinToAdd, user, match, addCoinToPortfolio }) => {
-  const emptyErrorState = { amountInvestedError: false, amountPurchasedError: false };
+const AddCoinPortfolioModal = ({
+  isVisible,
+  toggleVisible,
+  coinToAdd,
+  user,
+  match,
+  addCoinToPortfolio
+}) => {
+  const emptyErrorState = {
+    amountInvestedError: false,
+    amountPurchasedError: false
+  };
   const [inputErrors, updateInputErrors] = useState(emptyErrorState);
   const [amountPurchased, updateAmountPurchased] = useState(null);
   const [amountInvested, updateAmountInvested] = useState(null);
   const handleAddCoinToPortfolio = () => {
-    const numberChecker = /^[+]?([0-9]+(?:[\.][0-9]*)?|\.[0-9]+)$/;
-    const isAmountInvestedValid = numberChecker.test(amountInvested);
-    const isAmountPurchasedValid = numberChecker.test(amountPurchased);
+    const isAmountInvestedValid = NUMBER_ONLY_REGX.test(amountInvested);
+    const isAmountPurchasedValid = NUMBER_ONLY_REGX.test(amountPurchased);
     const payload = {
       accountKey: user.uid,
       portfolioKey: `-${match.params.id}`,
@@ -44,14 +54,11 @@ const AddCoinPortfolioModal = ({ isVisible, toggleVisible, coinToAdd, user, matc
         type="number"
         value={amountPurchased}
         onChange={amountPurchased => updateAmountPurchased(amountPurchased)}
-        handleSubmit={e => [
-          e.preventDefault(),
-          handleAddCoinToPortfolio()
-        ]}
+        handleSubmit={e => [e.preventDefault(), handleAddCoinToPortfolio()]}
         error={inputErrors.amountPurchasedError}
         helperText={
           inputErrors.amountPurchasedError
-            ? "Please Enter a valid Positive Number"
+            ? "Please Enter a Positive Number"
             : null
         }
         clearError={() => updateInputErrors(emptyErrorState)}
@@ -66,7 +73,7 @@ const AddCoinPortfolioModal = ({ isVisible, toggleVisible, coinToAdd, user, matc
         error={inputErrors.amountInvestedError}
         helperText={
           inputErrors.amountInvestedError
-            ? "Please Enter a valid Positive Number"
+            ? "Please Enter a Positive Number"
             : null
         }
         clearError={() => updateInputErrors(emptyErrorState)}
@@ -77,8 +84,8 @@ const AddCoinPortfolioModal = ({ isVisible, toggleVisible, coinToAdd, user, matc
 
 AddCoinPortfolioModal.defaultProps = {
   isVisible: false,
-  toggleVisible: () => console.log('Toogle Visible Not Declared.'),
-  coinToAdd: 'No Coin To Add Declared',
-}
+  toggleVisible: () => console.log("Toogle Visible Not Declared."),
+  coinToAdd: "No Coin To Add Declared"
+};
 
 export default AddCoinPortfolioModal;
